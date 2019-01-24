de9672cc874444a1086d4ee83e307bcf7e8c21c1
Administrator <Administrator@WIN-K0BGQH20469>
2019-01-24 10:53:11 +0900

    ss

--- SMS/src/main/java/com/pub/sms/controller/SmsSellBoController.java
+++ SMS/src/main/java/com/pub/sms/controller/SmsSellBoController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pub.sms.service.SmsSellBoService;
-
 @Controller
 public class SmsSellBoController {
 	@Autowired

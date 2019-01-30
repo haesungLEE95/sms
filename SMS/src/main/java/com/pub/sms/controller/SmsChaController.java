a72c1c73a61a05d1d117d87de430ffadde979606
Administrator <Administrator@WIN-K0BGQH20469>
2019-01-30 14:58:10 +0900

    ignore

--- SMS/src/main/java/com/pub/sms/controller/SmsChaController.java
+++ SMS/src/main/java/com/pub/sms/controller/SmsChaController.java
@@ -1,21 +1,14 @@
 package com.pub.sms.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import java.util.Collection;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.pub.sms.model.SmsCha;
 import com.pub.sms.model.SmsMainCate;
 import com.pub.sms.model.SmsMem;
-import com.pub.sms.model.SmsReq;
-import com.pub.sms.model.SmsSubCate;
+ import com.pub.sms.model.SmsSubCate;
 import com.pub.sms.service.SmsChaPagingBean;
 import com.pub.sms.service.SmsChaService;
 import com.pub.sms.service.SmsMCateService;
@@ -42,21 +35,16 @@ public class SmsChaController {
 		int endRow   = startRow + rowPerPage - 1;
 		smscha.setStartRow(startRow);
 		smscha.setEndRow(endRow);
-		
 		Collection<SmsCha> list = scs.list(smscha);
 		for(SmsCha sr : list) {
 			SmsMem sm = sms.memNick(sr.getMem_no());
 			sr.setNickname(sm.getNickname());			
 		}
-		
 		SmsChaPagingBean pb=new SmsChaPagingBean(currentPage,rowPerPage,total);
 		Collection<SmsMainCate> mcateList = smcs.list();
 		Collection<SmsSubCate> scateList = sscs.list();
 		model.addAttribute("mcateList", mcateList);
 		model.addAttribute("scateList", scateList);
-		//if (memNick != null) {
-		//model.addAttribute("memNick", memNick.getNickname());
-		//}
 		model.addAttribute("list", list);
 		model.addAttribute("smscha", smscha);
 		model.addAttribute("pageNum", pageNum);
@@ -76,7 +64,6 @@ public class SmsChaController {
 	public String smsInsert(String pageNum, SmsCha smscha, Model model) {
 		int result = 0;
 		result = scs.insert(smscha);
-		
 		model.addAttribute("result", result);
 		model.addAttribute("pageNum", pageNum);
 		return "/cha/smsInsert";

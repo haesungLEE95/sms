97171ac843f015a12b06da057b27310c3cb4e9b2
ereshi25 <indigtop@gmail.com>
2019-01-31 15:26:30 +0900

    maybe

--- SMS/src/main/java/com/pub/sms/controller/SmsMemController.java
+++ SMS/src/main/java/com/pub/sms/controller/SmsMemController.java
@@ -2,20 +2,27 @@ package com.pub.sms.controller;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.pub.sms.model.SmsCharge;
 import com.pub.sms.model.SmsMainCate;
 import com.pub.sms.model.SmsMem;
 import com.pub.sms.model.SmsSubCate;
+import com.pub.sms.service.SmsChargeService;
 import com.pub.sms.service.SmsMCateService;
 import com.pub.sms.service.SmsMemService;
 import com.pub.sms.service.SmsSCateService;
@@ -32,6 +39,8 @@ public class SmsMemController {
 	private UploadController upc;
 	@Autowired
 	BCryptPasswordEncoder pwEncoder;
+	@Autowired
+	private SmsChargeService scs;
 	
 	@RequestMapping("joinForm")
 	public String joinForm(Model model) {
@@ -96,8 +105,8 @@ public class SmsMemController {
 	public String idChk(String mem_id) {
 		String msg = "";
 		SmsMem mem = sms.select(mem_id);
-		if (mem==null) msg="<br>사용가능합니다";
-		else msg="<br>다른 아이디를 선택하세요";
+		if (mem==null) msg="사용가능합니다";
+		else msg="다른 아이디를 선택하세요";
 		return msg;
 	}
 	
@@ -108,8 +117,8 @@ public class SmsMemController {
 	public String nickChk(String nickname) {
 		String msg = "";
 		SmsMem mem = sms.nickSelect(nickname);
-		if (mem==null) msg="<br>사용가능합니다";
-		else msg="<br>다른 닉네임을 선택하세요";
+		if (mem==null) msg="사용가능합니다";
+		else msg="다른 닉네임을 선택하세요";
 		return msg;
 	}
 	
@@ -175,9 +184,117 @@ public class SmsMemController {
 		model.addAttribute("mem", mem);
 		return "/mem/myPage";
 	}
+
+	@RequestMapping("updateInfo")
+	public String updateInfo(Model model, SmsMem mem, String mem_id) {
+		mem = sms.select(mem_id);
+		model.addAttribute("mem", mem);
+		return "/mem/updateInfo";
+	}
+	/*	public String updateInfo(Model model, HttpSession session, SmsMem mem) {
+		int result =0;
+		String mem_id = (String) session.getAttribute("mem_id");
+		SmsMem smem = sms.select(mem_id); //}
+		smem = sms.select(mem.getMem_id());
+		if(smem != null) result = sms.updateInfo(mem);		
+		model.addAttribute("result", result);
+		model.addAttribute("mem", mem);	
+		return "/mem/updateInfo";
+	}*/
+	@RequestMapping("updateMem")
+	public String updateMem(Model model, SmsMem mem) {
+		int result = 0;
+		SmsMem smem = sms.select(mem.getMem_id());
+		
+		System.out.println("업데이트 데이터"+mem);
+		System.out.println("업데이트 데이터"+mem.getMem_id());
+		System.out.println("업데이트 데이터"+mem.getNickname());
+		System.out.println("업데이트 데이터"+mem.getPasswd());
+		System.out.println("업데이트 데이터"+mem.getAddr());
+		
+		if (mem.getPasswd()!="") {
+			String encPass = pwEncoder.encode(mem.getPasswd());
+			mem.setPasswd(encPass);
+			System.out.println("패스워드 변경됨");
+		}
+		else {mem.setPasswd(smem.getPasswd());
+			System.out.println("패스워드 그대로 : "+smem.getPasswd());
+		}
+		
+		try {
+			System.out.println("리절트값"+result);
+			result = sms.updateMem(mem);
+			System.out.println("리절트값"+result);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			result = -1;
+		}
+		model.addAttribute("result", result);
+		model.addAttribute("mem", mem);			
+		return "/mem/updateMem";
+	}
+	
+/*	@ModelAttribute("mnyMap")
+	protected Map mnyListMap(HttpServletRequest request) throws Exception {
+		Map<String, Integer> mnyMapp = new LinkedHashMap();
+		mnyMapp.put("2,500원",2500);
+		mnyMapp.put("5,000원",5000);
+		mnyMapp.put("10,000원",10000);
+		mnyMapp.put("30,000원",30000);
+		mnyMapp.put("50,000원",50000);
+		mnyMapp.put("100,000원",100000);
+
+		Map mnyMap = new HashMap();
+		mnyMap.put("mnyMap", mnyMapp);
+		return mnyMap;
+	}*/
+	
+	@RequestMapping("chargeMny")
+	public String chargeMny(Model model, int mem_no) {
+		SmsMem mem = sms.memNick(mem_no);
+		model.addAttribute("mem", mem);		
+		return "/mem/chargeMny";
+	}
+		
+	@RequestMapping("chargeMnyResult")
+	public String chargeMnyResult(Model model, SmsMem mem, int mny) {
+		System.out.println("어디까지 나오나 0 "+mny+" & "+mem.getMny());
+		SmsMem smem = sms.select(mem.getMem_id());
+		smem.setMny(mny);
+		System.out.println("어디까지 나오나 1 "+smem.getMny());
+		SmsCharge scg = new SmsCharge();
+		int result = 0;
+		try {
+			System.out.println("어디까지 나오나 2 "+mem.getPasswd());
+			String dbPw =  smem.getPasswd();
+			String rawPw = mem.getPasswd();
+			//dbPw : db에 저장된 암호화된 패스워드
+			//rawPw : 입력창에 입력된 실제 패스워드
+			if (smem==null) result = -1;
+			//멤버id가 매칭되지 않으면 result -1을 반환한다.
+			else if (pwEncoder.matches(rawPw, dbPw)) {
+				//matches메소드는 입력된 패스워드를 해독해서 암호화된 패스워드와 비교한다
+				//rawPw를 암호화한 값이 dbPw와 매치된다면 result 1을 반환한다
+				mem.setPasswd(dbPw);
+				result = sms.chargeMny(smem);
+				//멤버DB에 Mny를 insert
+				
+				scg.setMem_no(smem.getMem_no());
+				scg.setChg_mny(smem.getMny());
+				//int charge = scs.chargeMny(scg);
+				//충전내역DB에 회원번호와 충전금액 insert
+			}			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			result = -1;
+		}
+		model.addAttribute("result", result);		
+		return "/mem/chargeMnyResult";
+
 	@RequestMapping("logout")
 	public String logout(HttpSession session) {
 		session.invalidate();
 		return "/mem/logout";
+
 	}
 }
--- SMS/src/main/webapp/WEB-INF/views/mem/myPage.jsp
+++ SMS/src/main/webapp/WEB-INF/views/mem/myPage.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ include file="../header.jsp" %>
+	pageEncoding="UTF-8"%>
+<%@ include file="../header.jsp"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,28 +10,67 @@
 	$(function() {
 		$('#disp').load('profile.do?mem_id=${mem.mem_id}');
 	});
+/* 	$(document).ready(function() {
+		// test_btn 버튼의 쿠키값을 검사한다. -  값이 Y 인경우 testDiv 부분 open
+		if ($.cookie("utd_btn") == "Y") {
+			$("#dispUpdate").show();
+			$("#update");
+			//버튼이미지 변경
+		} else {
+			$("#dispUpdate").hide();
+			$("#update");
+		}
+	});
+ */
+ </script>
+ <script type="text/javascript">
+ function chargeMny() {
+		window.open("chargeMny.do?mem_no=${mem.mem_no}", "포인트 충전", "width=530, height=380, left=100, top=50"); 
+		}
+ var profImg 
 </script>
 <style type="text/css">
-.prof {width: 350px }
-#pimg{  width: 100px; height: 100px;
-    object-fit: cover;
+.prof {
+	width: 350px
+}
+#pimg {
+	width: 100px;
+	height: 100px;
+	object-fit: cover;
+}
+.btn btn-info {
+	width: 140px;
+	align-content: center;
+}
+.btn btn-success{
+	width: 160px;
+	align-content: center;
 }
 </style>
 </head>
 <body>
-<div class="container" align="center">
-	<table class="table table-striped">
-	<tr><th class="prof" rowspan="2">
-	<c:if test="${ !empty mem.mem_img }">
-	<img id="pimg" width="100px" height="100px"  src="upload/${mem.mem_img}"> 
-	</c:if><br>
-	${mem.nickname }님<br>환영합니다<br>
-	<div id="disp"></div>
-	</th><td>상단 통계 영역</td></tr>
-	<tr><td>내 게시글(?)</td></tr>
-	<tr><th>프로필 영역</th><td>내용</td></tr>
-	</table>
-	<a href="loginForm.do">로그아웃</a>
+	<div class="container" align="center">
+		<table class="table table-striped">
+			<tr>
+				<th class="prof" rowspan="2">
+					<c:if test="${ !empty mem.mem_img }">
+						<img id="pimg" width="100px" height="100px"
+							src="upload/${mem.mem_img}">
+					</c:if>	${mem.nickname }님<br>환영합니다<br>
+					<div id="disp"></div></th>
+				<td>내 포인트 : ${mem.mny }</td>
+			</tr>
+			<tr>
+				<td>내 게시글(?)</td>
+			</tr>
+			<tr>
+				<th><a href="updateInfo.do?mem_id=${mem.mem_id }" class="btn btn-info">개인정보 수정</a><br><br>
+				<input type="button" value="포인트 충전" onclick="chargeMny();" class="btn btn-success"></th>
+				<td>내용</td>
+			</tr>
+		</table>
+	</div>
+	<a href="logout.do">로그아웃</a>
 </div>
 </body>
 </html>

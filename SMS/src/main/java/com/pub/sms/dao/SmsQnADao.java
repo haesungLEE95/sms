90be18152ae85c7adab1dc485689cf41d36c37bf
Administrator <Administrator@WIN-K0BGQH20469>
2019-01-31 16:35:45 +0900

    삭재

--- SMS/src/main/java/com/pub/sms/dao/SmsQnADao.java
+++
@@ -1,9 +0,0 @@
-package com.pub.sms.dao;
-
-import com.pub.sms.model.SmsQnA;
-
-public interface SmsQnADao {
-
-	int insert(SmsQnA smsqna);
-
-}
--- SMS/src/main/java/com/pub/sms/service/SmsQnAService.java
+++
@@ -1,9 +0,0 @@
-package com.pub.sms.service;
-
-import com.pub.sms.model.SmsQnA;
-
-public interface SmsQnAService {
-
-	int insert(SmsQnA smsqna);
-
-}
